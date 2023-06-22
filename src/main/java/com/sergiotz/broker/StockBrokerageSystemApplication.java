bc578b97df89ad985605538845c1d2e13980590e
Sergio Tz <sergiotz@gmail.com>
2023-06-22 20:31:49 -0300

    initial code

---
+++ src/main/java/com/sergiotz/broker/StockBrokerageSystemApplication.java
@@ -0,0 +1,13 @@
+package com.sergiotz.broker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockBrokerageSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockBrokerageSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sergiotz/broker/StockBrokerageSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sergiotz.broker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockBrokerageSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
